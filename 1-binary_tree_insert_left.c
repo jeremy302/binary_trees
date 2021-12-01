825b8af02c0e6cd8a07e6de61c2f91f3bceb1167
jeremy302 <olajeremy123@gmail.com>
2021-12-01 13:52:04 +0100

    patch

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -12,8 +12,10 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 		return (printf("returning null\n"), NULL);
 	node->n = value;
 	node->left = parent->left;
+	if (parent->left)
+		parent->left->parent = node;
 	node->right = NULL;
 	parent->left = node;
-
+	node->parent = parent;
 	return node;
 }
