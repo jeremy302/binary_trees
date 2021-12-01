aba4e0d8d5466a54eaff7b032c78dac28d849ec6
jeremy302 <olajeremy123@gmail.com>
2021-12-01 13:54:18 +0100

    betty fix

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -1,5 +1,12 @@
 #include "binary_trees.h"
 
+/**
+ * binary_tree_insert_left - inserts a node as the left child
+ * @parent: parent node
+ * @: node's value
+ *
+ * Return: new node
+ */
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
 	binary_tree_t *node;
