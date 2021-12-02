8a9ca243d055816154c101b5e9f08ad803a38ebe
jeremy302 <olajeremy123@gmail.com>
2021-12-02 10:59:27 +0100

    patch

--- 101-binary_tree_levelorder.c
+++ 101-binary_tree_levelorder.c
@@ -10,30 +10,30 @@
  */
 void binary_tree_levelorder(const binary_tree_t *tree, void (*func)(int))
 {
-	binary_tree_t **level, **next_level, *node;
+	const binary_tree_t **level, **next_level, *node;
 	int i, j, level_len, next_level_len;
 
 	if (tree == NULL || func == NULL)
 		return;
-
-	level = calloc(1, sizeof(binary_tree_t *));
-	level_len = 1;
+	level = calloc(1, sizeof(binary_tree_t *)), level_len = 1;
 	if (level == NULL)
+	{
+		func(tree->n);
 		return;
-	level[0] = (binary_tree_t *)tree;
-	while (level != NULL && level_len > 0)
+	}
+	level[0] = tree;
+	while (1)
 	{
 		for (next_level_len = 0, i = 0; i < level_len; ++i)
 		{
-			node = level[i];
-			func(node->n);
-			next_level_len += (level[i]->left != NULL) +
-				(level[i]->right != NULL);
+			node = level[i], func(node->n);
+			next_level_len += (node->left != NULL) + (node->right != NULL);
 		}
-		next_level = calloc(next_level_len, sizeof(binary_tree_t *));
+		next_level = next_level_len ?
+			calloc(next_level_len, sizeof(binary_tree_t *)) : NULL;
 		if (next_level == NULL)
 		{
-			free(level);
+			free(level), free(next_level);
 			return;
 		}
 		for (j = 0, i = 0; i < level_len; ++i)
@@ -44,10 +44,6 @@ void binary_tree_levelorder(const binary_tree_t *tree, void (*func)(int))
 			if (node->right != NULL)
 				next_level[j++] = node->right;
 		}
-		free(level);
-		level = next_level;
-		level_len = next_level_len;
-		if (next_level_len == 0)
-			return;
+		free(level), level = next_level, level_len = next_level_len;
 	}
 }
